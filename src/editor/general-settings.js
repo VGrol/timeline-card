6f21dc5e972555fbc1074f8e9f6187828e9356c2
Kei van Grol <contact@vgrol.com>
2026-06-30 00:17:46 +0200

    Append NL Localisation general-settings.js

--- src/editor/general-settings.js
+++ src/editor/general-settings.js
@@ -70,6 +70,7 @@ class TimelineCardGeneralSettings extends LitElement {
                         { value: 'en-US', label: 'English (US)' },
                         { value: 'fr', label: 'Francais' },
                         { value: 'it', label: 'Italiano' },
+                        { value: 'nl', label: 'Nederlands' },
                         { value: 'pl', label: 'Polski' },
                         { value: 'pt-br', label: 'Portugues (BR)' },
                         { value: 'ru', label: 'Русский' },
